d94eb5baf20e79ba32d2709bfe68b912285eb985
aresrana <aresnov3@gmail.com>
2022-09-14 18:51:03 -0700

    Added multiple screens

---
+++ lib/screens/worship.dart
@@ -0,0 +1,139 @@
+///[Another example]
+import 'package:divya/services/song_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+
+import '../componet/mini_player.dart';
+import '../componet/song_widget.dart';
+import '../model/song.dart';
+
+class WorshipSongPage extends StatefulWidget {
+  const WorshipSongPage({Key? key}) : super(key: key);
+
+  @override
+  State<WorshipSongPage> createState() => _WorshipSongPage();
+}
+
+class _WorshipSongPage extends State<WorshipSongPage> {
+  int currentCategoryIndex = 0;
+  bool searching = false;
+  List<Song> _songList = [];
+  FocusNode focusNode = FocusNode();
+  @override
+  void initState() {
+    Provider.of<SongProvider>(context, listen: false)
+        .getSongsByCollection('Worship')
+        .then((value) {
+      setState(() {
+        _songList = value;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+
+    final provider = context.watch<SongProvider>();
+    return SafeArea(
+        child: Scaffold(
+            backgroundColor: Colors.grey[300],
+
+            bottomNavigationBar:
+            provider.playingSong != null ? const MiniPlayer() : null,
+            body: SingleChildScrollView(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                  SizedBox(
+                  height: 18,
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.36,
+                  width: MediaQuery.of(context).size.width * 0.7,
+                  decoration:  BoxDecoration(
+                    boxShadow: const [
+                    BoxShadow(
+                    color: Color.fromRGBO(0, 0, 0, 0.1),
+                      spreadRadius: 3.0,
+                      blurRadius: 6.0,
+                      offset: Offset(6,2)
+
+                    ),
+                      BoxShadow(
+                          color: Color.fromRGBO(192,192,192, 0.9),
+                          spreadRadius: 3.0,
+                          blurRadius: 6.0,
+                          offset: Offset( -6,-2)
+
+                      ),
+
+                    ],
+                      borderRadius: BorderRadius.all(Radius.circular(300)),
+                    border:Border.all(
+                        color:Colors.grey.shade300,
+                        width:10
+                    ),
+                      image: const DecorationImage(
+                          image: AssetImage('images/worship.jpg'),
+                          fit: BoxFit.cover),
+
+                  ),
+
+
+                ),
+                const SizedBox(
+                  height: 28,
+                ),
+                const Center(
+                  child: Text('Come, let us bow down in worship.',
+                      style: TextStyle(fontSize: 18)),
+                ),
+                const Center(
+                  child: Text('Psalm 95:6',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red)),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+
+
+                      provider.isLoading
+                          ? const CircularProgressIndicator()
+                          : ListView.builder(
+                        shrinkWrap: true,
+                        primary: false,
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        itemCount: _songList.length,
+                        itemBuilder: (context, index) {
+                          final song = _songList[index];
+                          return SongWidget(
+                            song: song,
+                            playingSongList: _songList,
+                          );
+                        },
+                      ),
+                      const SizedBox(height: 50),
+                    ]))
+          // _html(context)
+
+        ));
+  }
+
+//body
+
+}
